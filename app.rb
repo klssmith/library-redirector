39372eaff53c3a997a4a97777f32c60fedf820dc
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2017-07-03 14:32:53 +0100

    Add wildcard redirect page for all possible routes

--- app.rb
+++ app.rb
@@ -1,1 +1,21 @@
 require 'sinatra'
+
+get '/*' do
+  erb :index
+end
+
+post '/*' do
+  erb :index
+end
+
+put '/*' do
+  erb :index
+end
+
+patch '/*' do
+  erb :index
+end
+
+delete '/*' do
+  erb :index
+end
